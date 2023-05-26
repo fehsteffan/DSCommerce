cc986150f6656be6429929eefeca50be61d0739c
Felipe Steffan <felip.developer@hotmail.com>
2023-05-25 22:14:01 -0300

    PUT product

--- src/main/java/com/program/ecommerce/services/ProductService.java
+++ src/main/java/com/program/ecommerce/services/ProductService.java
@@ -35,22 +35,33 @@ public class ProductService {
 	
 	public ProductDTO insert(ProductDTO dto) {		
 		Product entity = new Product();
+		copyDtoToEntity(dto, entity);		
+		entity = productRepository.save(entity);		
+		return new ProductDTO(entity);	
+		
+		
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product entity = productRepository.getReferenceById(id);		
+		copyDtoToEntity(dto, entity);		
+		entity = productRepository.save(entity);		
+		return new ProductDTO(entity);	
+		
+				
+	}
+
+
+	@Transactional
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
 		
-		entity = productRepository.save(entity);		
-		return new ProductDTO(entity);
+	}		
 		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-
 }
+
+
